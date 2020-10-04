711a262f1b378c503bf8caa0ff413c2a4e247c35
Maxim Kovalchuk <maksimkovalcuk81@gmail.com>
2020-10-04 17:01:48 +0300

    Added Constants static class

---
+++ src/main/java/com/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.constants;
+
+public class Constants {
+    public static final String UNDEFINED = "undefined";
+    public static final String FUNC_F = "funcF";
+    public static final  String FUNC_G = "funcG";
+}
