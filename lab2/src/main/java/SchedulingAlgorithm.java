96ce3321aa62d0dccb24989cee2fa743b4ff54f1
makcimmushka <mkovalchuk@magisoft.com>
2020-11-29 23:32:44 +0200

    Implemented robin round algo

--- lab2/src/main/java/SchedulingAlgorithm.java
+++ lab2/src/main/java/SchedulingAlgorithm.java
@@ -1,88 +1,146 @@
-// Run() is called from Scheduling.main() and is where
+// Run() is called from scheduling.Scheduling.main() and is where
 // the scheduling algorithm written by the user resides.
 // User modification should occur within the Run() function.
+// ;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Vector;
 import java.io.*;
 
+
 public class SchedulingAlgorithm {
 
+  private enum ProcessState {
+    REGISTERED,
+    COMPLETED,
+    JOINED,
+    FORCEDOUT
+  }
+
+  private static void print(int time, PrintStream out, ProcessState state, sProcess process, Vector<sProcess> processVector) {
+    print(time, out, state, process, processVector, false);
+  }
+
+  private static void print(int time, PrintStream out, ProcessState state, sProcess process, Vector<sProcess> processVector, boolean quantumExpired) {
+    out.println(time + "ms ");
+    switch (state) {
+      case REGISTERED:
+        out.println("Process: " + processVector.indexOf(process) + " registered... (" + process.cpuTime + " " + process.delay + " " + process.cpuDone + ")");
+        break;
+      case COMPLETED:
+        if (quantumExpired) {
+          out.println("Quantum time expired");
+        } else {
+          out.println("Quantum time reset");
+        }
+        out.println("Process: " + processVector.indexOf(process) + " completed... (" + process.cpuTime + " " + process.delay + " " + process.cpuDone + ")");
+        break;
+      case JOINED:
+        out.println("Process joined queue");
+        out.println("Process: " + processVector.indexOf(process) + " (" + process.cpuTime + " " + process.delay + " " + process.cpuDone + ")");
+        break;
+      case FORCEDOUT:
+        out.println("Quantum time expired");
+        out.println("Process: " + processVector.indexOf(process) + " forced out... (" + process.cpuTime + " " + process.delay + " " + process.cpuDone + ")");
+        break;
+    }
+
+  }
+
   public static Results Run(int quantum, int runtime, Vector<sProcess> processVector, Results result) {
-    int i = 0;
     int comptime = 0;
-    int currentProcess = 0;
-    int previousProcess = 0;
     int size = processVector.size();
     int completed = 0;
+
     String resultsFile = "Summary-Processes";
 
-    result.schedulingType = "Batch (Nonpreemptive)";
-    result.schedulingName = "Round-robin";
-    try {
-      //BufferedWriter out = new BufferedWriter(new FileWriter(resultsFile));
-      //OutputStream out = new FileOutputStream(resultsFile);
-      PrintStream out = new PrintStream(new FileOutputStream(resultsFile));
-      sProcess process = processVector.elementAt(currentProcess);
-      out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " +
-              process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+    Vector<sProcess> sortedProcessVector = (Vector<sProcess>) processVector.clone();
+    sortedProcessVector.sort((o1, o2) -> {
+      if (o1.delay == o2.delay) {
+        return 0;
+      } else if (o1.delay > o2.delay) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
 
+    Queue<sProcess> queue = new LinkedList<>();
+
+    result.schedulingType = "Batch (Nonpreemptive)";
+    result.schedulingName = "Round Robin scheduling";
+
+    int lastProcessIndex = 0;
+    sProcess process = null;
+    int quantumCounter = 0;
+
+    try {
+      PrintStream out = new PrintStream(new FileOutputStream(resultsFile));
       while (comptime < runtime) {
-        if (process.cpudone == process.cputime) {
+        //add entered processes
+        if (lastProcessIndex < size) {
+          while (sortedProcessVector.elementAt(lastProcessIndex).delay == comptime) {
+            sProcess processCopy = sortedProcessVector.elementAt(lastProcessIndex);
+            queue.add(processCopy);
+            print(comptime, out, ProcessState.JOINED, processCopy, processVector);
+            ++lastProcessIndex;
+            if (lastProcessIndex >= size) {
+              break;
+            }
+          }
+        }
+
+        if (process == null && !queue.isEmpty()) {
+          process = queue.peek();
+          print(comptime, out, ProcessState.REGISTERED, process, processVector);
+        }
+
+        if (process != null && process.cpuDone == process.cpuTime) {
           completed++;
-          out.println("Process: " + currentProcess + " completed... (" + process.cputime + " " +
-                  process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+
+          print(comptime, out, ProcessState.COMPLETED, process, processVector, quantumCounter == quantum);
+
+          quantumCounter = 0;
 
           if (completed == size) {
             result.compuTime = comptime;
-            out.close();
             return result;
           }
 
-          for (i = size - 1; i >= 0; i--) {
-            process = processVector.elementAt(i);
+          queue.remove();
 
-            if (process.cpudone < process.cputime) { 
-              currentProcess = i;
-            }
+          if (!queue.isEmpty()) {
+            process = queue.peek();
+            print(comptime, out, ProcessState.REGISTERED, process, processVector);
+          } else {
+            process = null;
           }
-
-          process = processVector.elementAt(currentProcess);
-          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " "
-                  + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
         }
 
-        if (process.ioblocking == process.ionext) {
-          out.println("Process: " + currentProcess + " I/O blocked... (" + process.cputime + " " +
-                  process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+        //if quantum time expired
+        if (quantumCounter == quantum) {
+          quantumCounter = 0;
+          process.numBlocked++;
 
-          process.numblocked++;
-          process.ionext = 0; 
-          previousProcess = currentProcess;
+          print(comptime, out, ProcessState.FORCEDOUT, process, processVector, true);
 
-          for (i = size - 1; i >= 0; i--) {
-            process = processVector.elementAt(i);
+          queue.add(queue.poll());
+          process = queue.peek();
 
-            if (process.cpudone < process.cputime && previousProcess != i) { 
-              currentProcess = i;
-            }
-          }
-
-          process = processVector.elementAt(currentProcess);
-          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " +
-                  process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
+          print(comptime, out, ProcessState.REGISTERED, process, processVector);
         }
 
-        process.cpudone++;
-
-        if (process.ioblocking > 0) {
-          process.ionext++;
+        if (process != null) {
+          quantumCounter++;
+          process.cpuDone++;
         }
 
         comptime++;
       }
 
       out.close();
-    } catch (IOException e) {
+    } catch (FileNotFoundException e) {
       e.printStackTrace();
     }
 
